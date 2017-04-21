59e7545b9242eabc237a80f832d32c2b16456d6f
szsncs <787239289@qq.com>
2017-04-21 15:24:19 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,14 +1,346 @@
 
+
 var https="https://szsncs.github.io/an/js/"
 var js=".js";
-function getIndex_JS(){
-	var index_js = [https+"jquery-2.1.4.min"+js,
-			https+"summer"+js,
-			https+"index"+js,
-			https+"Frameworks/iuapmobile.frameworks.core"+js,
-			https+"Frameworks/iuapmobile.frameworks.ui"+js];
-	return index_js;				
+var jquery_2_1_4="jquery-2.1.4.min";
+var jquery="jquery.min";
+var summer="summer";
+var frameworks_core="Frameworks/iuapmobile.frameworks.core";
+var frameworks_ui="Frameworks/iuapmobile.frameworks.ui";
+var islider="islider";
+var knockout="knockout";
+var knockout_2_2_0="knockout-2.2.0";
+var frameworks_listview="Frameworks/iuapmobile.frameworks.listview";
+var frameworks_listgroup="Frameworks/iuapmobile.frameworks.listgroup-2.7.0";
+var frameworks_sortlist="Frameworks/iuapmobile.frameworks.sortlist-2.7.0";
+var common="common";
+var font="font";
+
+var yonyou_js=[summer,frameworks_core,frameworks_ui];
+
+
+function getEquipmentMaintenanceInfo_js(url){//引入已到饲料查询js	7
+	var equipmentMaintenanceInfo_js = [https+jquery+js,
+					https+url+js
+					];
+	return equipmentMaintenanceInfo_js.concat(yonyou_js);				
 }
+
+function getEquipmentMaintenance_js(url){//引入已到饲料查询js	7
+	var equipmentMaintenance_js = [https+jquery+js,
+					https+url+js
+					];
+	return equipmentMaintenance_js.concat(yonyou_js);				
+}
+
+function getEquipmentFirstList_js(url){//引入已到饲料查询js	7
+	var equipmentFirstList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return equipmentFirstList_js.concat(yonyou_js);				
+}
+
+function getEateryaccountInfo_js(url){//引入已到饲料查询js	7
+	var eateryaccountInfo_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js,
+					https+frameworks_sortlist+js
+					];
+	return eateryaccountInfo_js.concat(yonyou_js);				
+}
+
+function getEateryaccountFirstList_js(url){//引入已到饲料查询js	7
+	var eateryaccountFirstList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return eateryaccountFirstList_js.concat(yonyou_js);				
+}
+
+function getEateryaccount_js(url){//引入已到饲料查询js	7
+	var eateryaccount_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js,
+					https+frameworks_sortlist+js
+					];
+	return eateryaccount_js.concat(yonyou_js);				
+}
+
+function getConsumableList_js(url){//引入已到饲料查询js	7
+	var consumableList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return consumableList_js.concat(yonyou_js);				
+}
+
+function getConsumableInfo_js(url){//引入已到饲料查询js	7
+	var consumableInfo_js = [https+jquery+js,
+					https+url+js
+					];
+	return consumableInfo_js.concat(yonyou_js);				
+}
+
+function getConsumable_js(url){//引入已到饲料查询js	7
+	var consumable_js = [https+jquery+js,
+					https+url+js
+					];
+	return consumable_js.concat(yonyou_js);				
+}
+
+function getWeekWeighing_js(url){//引入已到饲料查询js	7
+	var weekWeighing_js = [https+jquery+js,
+					https+url+js
+					];
+	return weekWeighing_js.concat(yonyou_js);				
+}
+
+function getUseGas_js(url){//引入已到饲料查询js	7
+	var useGas_js = [https+jquery+js,
+					https+url+js
+					];
+	return useGas_js.concat(yonyou_js);				
+}
+
+function getUseElectricity_js(url){//引入已到饲料查询js	7
+	var useElectricity_js = [https+jquery+js,
+					https+url+js
+					];
+	return useElectricity_js.concat(yonyou_js);				
+}
+
+function getSecondList_js(url){//引入已到饲料查询js	7
+	var secondList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return secondList_js.concat(yonyou_js);				
+}
+
+function getPluckFoodWater_js(url){//引入已到饲料查询js	7
+	var pluckFoodWater_js = [https+jquery+js,
+					https+url+js
+					];
+	return pluckFoodWater_js.concat(yonyou_js);				
+}
+
+function getInChick_js(url){//引入已到饲料查询js	7
+	var inChick_js = [https+jquery+js,
+					https+url+js
+					];
+	return inChick_js.concat(yonyou_js);				
+}
+
+function getEstimateAlive_js(url){//引入已到饲料查询js	7
+	var estimateAlive_js = [https+jquery+js,
+					https+url+js
+					];
+	return estimateAlive_js.concat(yonyou_js);				
+}
+
+function getDeathElimination_js(url){//引入已到饲料查询js	7
+	var deathElimination_js = [https+jquery+js,
+					https+url+js
+					];
+	return deathElimination_js.concat(yonyou_js);				
+}
+
+function getDailyReportList_js(url){//引入已到饲料查询js	7
+	var dailyReportList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return dailyReportList_js.concat(yonyou_js);				
+}
+
+function getMedicineReceive_js(url){//引入已到饲料查询js	7
+	var medicineReceive_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js,
+					https+frameworks_sortlist+js
+					];
+	return medicineReceive_js.concat(yonyou_js);				
+}
+
+function getMedicineList_js(url){//引入已到饲料查询js	5
+	var medicineList_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return medicineList_js.concat(yonyou_js);				
+}
+
+
+function getMedicineConfirmation_js(url){//引入已到饲料查询js	7
+	var medicineConfirmation_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js,
+					https+frameworks_sortlist+js
+					];
+	return medicineConfirmation_js.concat(yonyou_js);				
+}
+
+function getLoimialist_js(url){
+	var loimialist_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return loimialist_js.concat(yonyou_js);				
+}
+
+function getLoimiaInfo_js(url){
+	var loimiaInfo_js = [https+jquery+js,
+					https+url+js,
+					https+common+js,
+					https+font+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return loimiaInfo_js.concat(yonyou_js);				
+}
+
+function getList_js(url){
+	var list_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return list_js.concat(yonyou_js);				
+}
+
+function getIllnessChange_js(url){
+	var illnessChange_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return illnessChange_js.concat(yonyou_js);				
+}
+
+function getSilverskinConfirmation_js(url){
+	var silverskinConfirmation_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					];
+	return silverskinConfirmation_js.concat(yonyou_js);				
+}
+
+function getSilverskinArrived_js(url){
+	var silverskinArrived_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return silverskinArrived_js.concat(yonyou_js);				
+}
+
+function getNotApprvoeSuppliesInfo_js(url){
+	var notApprvoeSuppliesInfo_js = [https+jquery+js,
+					https+utl+js
+					];
+	return notApprvoeSuppliesInfo_js.concat(yonyou_js);				
+}
+
+function getNotApprvoeSupplies_js(url){
+	var notApprvoeSupplies_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return notApprvoeSupplies_js.concat(yonyou_js);				
+}
+
+function getTransferSummary_js(url){
+	var transferSummary_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return transferSummary_js.concat(yonyou_js);				
+}
+
+
+function getFeedTransfer_js(url){
+	var feedTransfer_js = [https+jquery+js,
+					https+utl+js
+					];
+	return feedTransfer_js.concat(yonyou_js);				
+}
+
+function getFeedSummary_js(url){
+	var feedSummary_js = [https+jquery_2_1_4+js,
+					https+jquery+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return feedSummary_js.concat(yonyou_js);				
+}
+
+function getFeedDetails_js(url){
+	var feedDetails_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					];
+	return feedDetails_js.concat(yonyou_js);				
+}
+
+function getFeedConfirmation_js(url){
+	var feedConfirmation_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					];
+	return feedConfirmation_js.concat(yonyou_js);				
+}
+
+function getLoginSetting_js(url){
+	var loginSetting_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					];
+	return loginSetting_js.concat(yonyou_js);				
+}
+
+function getFeedApply_js(url){
+	var feedApply_js = [https+jquery+js,
+					https+utl+js
+					];
+	return feedApply_js.concat(yonyou_js);				
+}
+
+function getFeedArrived_js(url){
+	var feedArrived_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return feedArrived_js.concat(yonyou_js);				
+}
+
+function getMain_js(url){//引入已到饲料查询js	5
+	var main_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					https+knockout_2_2_0+js,
+					https+frameworks_listgroup+js
+					];
+	return main_js.concat(yonyou_js);				
+}
+
+function getIndex_js(url){//引入登录界面js	2
+	var index_js = [https+jquery_2_1_4+js,
+					https+url+js,
+					];
+	return index_js.concat(yonyou_js);				
+}
+
 
 /**
  * 调用接口方法 对$summer.callAction({})的封装 
