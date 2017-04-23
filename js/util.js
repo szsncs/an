5516ed4702a552ea0f00ffc63c4764f33302529f
szsncs <787239289@qq.com>
2017-04-23 16:00:35 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -442,6 +442,7 @@ function lastPageRefresh(jsfun,url,html){
 	});
 }
 
+
 /**
  * 四舍五入
  */
@@ -453,6 +454,50 @@ function round(num,d){
 	//Step：返回num缩小10的d次方倍，获得最终结果
 	return num/Math.pow(10,d);
 }
+
+/**
+ * 原生ajax 
+ * @param {Object} opt
+ */
+ function ajax(opt) {
+          opt = opt || {};
+         opt.method = opt.method.toUpperCase() || 'POST';
+         opt.url = opt.url || '';
+         opt.async = opt.async || true;
+         opt.data = opt.data || null;
+         opt.success = opt.success || function () {};
+         var xmlHttp = null;
+         if (XMLHttpRequest) {
+             xmlHttp = new XMLHttpRequest();
+         }
+         else {
+             xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+         }var params = [];
+         for (var key in opt.data){
+             params.push(key + '=' + opt.data[key]);
+         }
+         var postData = params.join('&');
+         if (opt.method.toUpperCase() === 'POST') {
+             xmlHttp.open(opt.method, opt.url, opt.async);
+             xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+             xmlHttp.send(postData);
+         }
+         else if (opt.method.toUpperCase() === 'GET') {
+             xmlHttp.open(opt.method, opt.url + '?' + postData, opt.async);
+             xmlHttp.send(null);
+         } 
+         xmlHttp.onreadystatechange = function () {
+             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                 opt.success(xmlHttp.responseText);
+             }
+         };
+     }
+
+
+
+
+
+
 
 var https_css="https://szsncs.github.io/an/css/";
 var szsn_app = "szsn-app/";
