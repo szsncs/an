bd089607386fc156b3a835c36973a91f798e82c3
szsncs <787239289@qq.com>
2017-04-23 11:37:25 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,6 +1,5 @@
 
-//var https="https://szsncs.github.io/an/js/"
-var https="ZSYZ/js/"
+var https="https://szsncs.github.io/an/js/"
 var js=".js";
 var jquery_2_1_4="jquery-2.1.4.min";
 var jquery="jquery.min";
