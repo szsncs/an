1eace04a947bb28dd614e733bf3193c0090b97fa
szsncs <787239289@qq.com>
2017-04-24 18:46:07 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -562,6 +562,7 @@ function isOs(){
         };
     }
 
+
 /**
  * 版本升级
  */
@@ -639,7 +640,6 @@ function ingVersion(){
             }
         });
     }
-
 
 
 /**
@@ -1125,23 +1125,31 @@ function getIndex_css(){
 	return index_css;				
 }
 window.onload = function(){
+	//失去焦点是触发
+	$(".focusevet").bind("blur", function() {
+		if($(this).val()==""){$(this).val("0")}
+	});
+	//获取焦点是触发
+	$(".focusevet").bind("focus", function() {
+		if($(this).val()=="0"){$(this).val("")}
+	});
 	$("input[type='number']").on('keyup', function(event) {
-			var $amountInput = $(this);
-			//响应鼠标事件，允许左右方向键移动
-			event = window.event || event;
-			if (event.keyCode == 37 | event.keyCode == 39) {
-				return;
-			}
-			//先把非数字的都替换掉，除了数字和.
-			$amountInput.val($amountInput.val().replace(/[^\d.]/g, "").
-			//只允许一个小数点
-			replace(/^\./g, "").replace(/\.{2,}/g, ".").
-			//只能输入小数点后两位
-			replace(".", "$#$").replace(/\./g, "").replace("$#$", ".").replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3'));
-		});
-		$("input[type='number']").on('blur', function() {
-			var $amountInput = $(this);
-			//最后一位是小数点的话，移除
-			$amountInput.val(($amountInput.val().replace(/\.$/g, "")));
-		});
+		var $amountInput = $(this);
+		//响应鼠标事件，允许左右方向键移动
+		event = window.event || event;
+		if (event.keyCode == 37 | event.keyCode == 39) {
+			return;
+		}
+		//先把非数字的都替换掉，除了数字和.
+		$amountInput.val($amountInput.val().replace(/[^\d.]/g, "").
+		//只允许一个小数点
+		replace(/^\./g, "").replace(/\.{2,}/g, ".").
+		//只能输入小数点后两位
+		replace(".", "$#$").replace(/\./g, "").replace("$#$", ".").replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3'));
+	});
+	$("input[type='number']").on('blur', function() {
+		var $amountInput = $(this);
+		//最后一位是小数点的话，移除
+		$amountInput.val(($amountInput.val().replace(/\.$/g, "")));
+	});
 };
