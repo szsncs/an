54422ba08cae44c3c4fc050605f04f15c4c6e24d
szsncs <787239289@qq.com>
2017-04-24 20:06:10 +0800

    Add files via upload

---
+++ js/feed/feedAddToTower.js
@@ -0,0 +1,40 @@
+var feedAddToTower = {
+	data:{},
+	viewid : "com.sunnercn.silverskin.FeedAddToTowerController",
+	init:function(){
+			$(".allTower li").hide();
+			$(".btn").on('click',function(){
+				$(this).parent().parent().find("ul li").stop().slideToggle(400);
+			});
+			//$(".ti-plus").on();
+			var data = $cache.read("logininfo");
+			var lonininfo = JSON.parse(data);
+			var json = {
+				logininfo:lonininfo
+			}
+			summer.showProgress({
+	            "title" : "加载中..."
+	        });
+	        callAction(silverskinArrived.viewid,"addFeedInfoInit",json,"callBack");
+	},
+	bindEvent:function(){
+		$(".um-back").click(function(){
+			summer.closeWin();
+		});
+		
+		$(".ti-home").unbind().on("click",function(){
+			//保存按钮
+			summer.openWin({
+                "id" : 'home',
+                "url" : 'html/main.html'
+            });
+		});
+		$(".ti-home").click(function(){
+			summer.closeWin();
+		});
+
+	},
+}
+summerready = function(){
+	feedAddToTower.init();
+};
