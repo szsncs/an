4a4250e060fc0701736744c013159801293ea5cb
szsncs <787239289@qq.com>
2017-04-27 21:38:50 +0800

    Update feedAddToTower.js

--- js/feed/feedAddToTower.js
+++ js/feed/feedAddToTower.js
@@ -1,73 +1,109 @@
-
-var feedDetails = {
+var feedAddToTower = {
 	data:{},
-	viewid : "com.sunnercn.feed.TransferEntryController",
+	viewid : "com.sunnercn.feed.FeedAddToTowerController",
 	init:function(){
-		var groupIndex=summer.pageParam ;
-		var arriveData = $cache.read("arriveData");
 		var data = $cache.read("logininfo");
-		var lonininfo = JSON.parse(data);
-		if(!groupIndex || !arriveData){
-			alert("数据出错");
-		}else{
-			var arriveDataObj = JSON.parse(arriveData);
-			if(arriveDataObj.billinfo.applyinfo.length> groupIndex.count){
-				var pk_apply = arriveDataObj.billinfo.applyinfo[groupIndex.count].pk_apply;
-				var billinfo={
-					pk_apply:pk_apply
-				}
-				var json= {
-					logininfo:lonininfo,
-					billinfo:billinfo
-				}
-				summer.showProgress({
-		            "title" : "加载中..."
-		        });
-		        callAction(feedDetails.viewid,"detailInfo",json,"callBack");
-			}
+		var logininfo = JSON.parse(data);
+		var json = {
+			logininfo:logininfo
 		}
+		summer.showProgress({
+	           // "title" : "加载中..."
+	    });
+	    callAction(feedAddToTower.viewid,"addFeedInfoInit",json,"callBack");
 	},
 	bindEvent:function(){
-		$(".um-back").click(function(){
+		$(".um-back").unbind().on('click',function(){
 			summer.closeWin();
 		});
-		$("#feedConfirm").unbind().on("click",function(){
-			//保存按钮
-			feedDetails.confirmClick();	
+		$(".allTower li").hide();
+		$(".btn").unbind().on('click',function(){
+			$(this).parent().next().find("li").stop().slideToggle(400);
 		});
-		$(".ti-home").unbind().on("click",function(){
-			//保存按钮
-			summer.openWin({
-                "id" : 'home',
-                "url" : 'html/main.html'
-            });
+		$(".ti-plus").unbind().on('click',function(){
+			// to do 行数限制
+			var logininfo = $cache.read("logininfo");
+			var json = JSON.parse(logininfo);
+			var siloinfoList = json.siloinfo;
+			var size = $(this).parent().parent().parent().find(".tower").length;
+			var li = "<li class='towers'>";
+			li+=$($(".towers")[0]).html();
+			li+="</li>";
+			$li=$(li);
+			$($li.find(".siloSelect")[0].options[size]).attr("selected","selected");
+			size<siloinfoList.length?$(this).parent().parent().before($li):UM.alert("没有更多料塔！");
+			$("input[type='number']").on('blur', feedAddToTower.calNum);
 		});
-		
-
+		$(".ti-minus").unbind().on('click',function(){
+			// to do 行数限制
+			var lis = $(this).parent().parent().parent().find("li");
+			lis.length>2?lis[lis.length-2].remove():UM.alert("对不起！请至少选一个料塔！");
+			$($(this).parent().parent().parent().children("li").find("input[type=number]")[0]).blur();
+		});
+		$("input[type='number']").on('blur', feedAddToTower.calNum);
+		$("#btn-save").unbind().on('click',feedAddToTower.confirmAddFeed);
+		$(".sk_refresh").unbind().on('click',feedAddToTower.init);
 	},
-	confirmClick :function(){
+	calNum:function(){
+		var oldnum = $(this).parent().parent().parent().parent().children("li").find("#readyToTower");
+		var readyToTower = 0.0;
+		input = $(this).parent().parent().parent().children(".towers").find("input[type=number]");
+		for(var i=0;i<input.length;i++){
+			readyToTower = eval(readyToTower+parseFloat(input[i].value?input[i].value:0));
+		}
+		oldnum.text(readyToTower);
+	},
+	confirmAddFeed:function(){
 		var array = [];
-		$(".applyli").each(function(){
-			var pk_inbill= $(this).attr("pk_inbill");
-			var i = $(this).attr("listNum");
-			var str = "#select"+i+" option:selected";
-			var pk_silo = $(str).attr("pk_silo");
-		    var silo_name=$("#select"+i).val();
-		    if( pk_silo && pk_silo.length>=0){
-		    	for(var i =0;i<feedDetails.data.length;i++){
-		    		if(feedDetails.data[i].pk_inbill ==pk_inbill){
-		    			if(feedDetails.data[i].silo_name !=silo_name){
-			    			var obj={
-								pk_inbill:pk_inbill,
-								pk_silo:pk_silo,
-								silo_name:silo_name
-							}
-							array.push(obj);
-		    			}
-		    		}
-		    	}
-		    }
-			
+		$("input[type=checkbox]:checked").each(function(){
+			var pk_inbill = $(this).attr("pk_inbill");
+			var div_bo = $(this).parent().parent().parent().parent();
+			var silos = [];
+			var carno = div_bo.find("#carno").text();//车牌号
+			var notInTower = parseFloat(div_bo.find("#notInTower").text().trim());//未打料量
+			var readyToTower = parseFloat(div_bo.children("li").find("#readyToTower").text());//准备打料总量
+			var allnuminput = div_bo.children("ul").find("input[type=number]");
+			allnuminput.each(function(){
+				if(this.value==0){
+					UM.alert("对不起！"+carno+"车,不允许填0或者空值！");
+					return confirmAddFeed();
+				}
+			});
+			if(notInTower<readyToTower){
+				UM.alert("对不起！"+carno+"车,打料数量超过未打料数量！");
+				return confirmAddFeed();
+			}
+			div_bo.children("ul").find(".siloSelect option:selected").each(function(){
+				if(!$(this).attr("pk_silo")){
+					UM.alert("对不起！"+carno+"车,请选择料塔！");
+					return confirmAddFeed();
+				}
+				var obj = {
+					pk_silo:$(this).attr("pk_silo"),
+					num:$(this).parent().parent().parent().find(".num input").val()
+				};
+				silos.push(obj);
+			});
+			if(silos.length>0){
+				var json={};
+				var count=0;
+				for(var i=0;i<silos.length;i++){
+					var pk = silos[i].pk_silo;
+					if(!json[pk]){
+						json[pk]=pk;
+						count++;
+					}
+				}
+				if(silos.length!=count){
+					UM.alert("对不起！"+carno+"车,不允许出现相同料塔！");
+					return confirmAddFeed();
+				}
+				var obj = {
+					pk_inbill:pk_inbill,
+					silos:silos
+				}
+				array.push(obj);
+			}
 		});
 		if(array.length>0){
 			var data = $cache.read("logininfo");
@@ -76,102 +112,122 @@ var feedDetails = {
 				billinfo:array,
 				logininfo:lonininfo
 			}
-			summer.showProgress({
-	            "title" : "加载中..."
-	        });
-	        var groupIndex=summer.pageParam ;
-			var arriveData = $cache.read("arriveData");
-			if(!groupIndex || !arriveData){
-				//确认收货 请求
-				callAction(feedDetails.viewid,"confirm",json,"confirmcallBack");
-			}else{
-				//已到货查询修改请求
-				callAction(feedDetails.viewid,"update",json,"updatecallBack");
-			}
+			//确认打料 请求
+			UM.confirm({
+                "title" : "打料确认",
+                "text" : "确认后打料数量将无法修改,确认吗？",
+                "btnText" : ["取消","确认"],
+                "overlay" : true,
+                "cancle" :function(){
+			        return;
+                },
+                "ok" :function(){
+                    summer.showProgress({"title" : "加载中..."});
+                    callAction(feedAddToTower.viewid,"confirmAddFeedToTower",json,"confirmcallBack");
+                }
+            });
 		}else{
-			alert("请先选择料塔！");
-		}
-		
-	},
-	loadPage:function(data){
-		var inbill = data.billinfo.inbill;
-		if(inbill.length>0){
-			feedDetails.initBillList(inbill);
-		}else{
-			alert("无物流将要到货");
+			UM.alert("请先选择车辆！");
 		}
 	},
-	initBillList:function(list){
-		$("#inbillListul").html("");
-		var html = "";
-		for(var i=0;i<list.length;i++){
-			html +='<li class="applyli" listNum ='+i+' pk_inbill='+list[i].pk_inbill +'><div class="um-list-item">'
-				 + '	 <div class="um-list-item-inner">'
-				 + '	 	<div class="um-list-item-left" style="width: 50%">'
-				 + '			<div class="che">车牌号：<span>'+list[i].carno+'</span></div>'
-				 + '				<div class="feed">料号：<span>'+list[i].feed_type_name+'</span></div>'
-				 + '				<div class="zhong">数量：<span select="select'+i+'" feed_silo_name="'+list[i].silo_name+'" id="'+list[i].carno+'">'+list[i].num+'吨</span></div>'
-				 + '		</div>'
-				 + '		<div class="um-list-item-right">'
-				 + '			<select id="select'+i+'"  class ="siloSelect" style="height:25px;"></select>'
-				 + '		</div>'
-				 + ' </div></div>'
-				 + '</li>'
-		}
-		$("#inbillListul").html(html);
-		feedDetails.initsiloSelect(list);
-	},
-	initsiloSelect:function(list){
+	initsiloSelect:function(){
 		var logininfo = $cache.read("logininfo");
 		var json = JSON.parse(logininfo);
 		var siloinfoList = json.siloinfo;
-		for(var j=0;j<list.length;j++){
-			$("#select"+j).html("");
-			var optionhtml= '<option>-请选择料塔-</option>';
-			for(var i=0;i<siloinfoList.length;i++){
-				optionhtml+='<option pk_silo="'+siloinfoList[i].pk_silo+'" '+list[j].cdeptid==siloinfoList[i].pk_silo?'selected="selected"':' '+'>'+siloinfoList[i].silo_name+'</option>';
-			}	
-			$("#select"+j).append(optionhtml);
+		$(".siloSelect").html("");
+		var optionhtml= '';
+		for(var i=0;i<siloinfoList.length;i++){
+			optionhtml+='<option pk_silo="'+siloinfoList[i].pk_silo+'">'+siloinfoList[i].silo_name+'</option>'
 		}
+		$(".siloSelect").append(optionhtml);
 	},
-};
+	loadPage:function(data){
+		$("#inbillListul").html("");
+		var html = "";
+		var inbill = data.billinfo.inbill;
+		if(inbill.length>0){
+			for(var i=0;i<inbill.length;i++){
+				html+='<div class="bo">'
+					+'<li class="um-list-item">'
+					+'<a href="javascript:;" class="btn">'
+					+'<label class="um-check-inline">'
+                    +'<input name="um-checkbox-inline" type="checkbox" pk_inbill="'+inbill[i].pk_inbill+'">'
+                   	+'<span class="um-icon-checkbox um-css3-vc"></span>'
+                    +'</label>'
+					+'<div class="um-media-body">'
+					+'<h4>车牌号：<span id="carno">'+inbill[i].carno+'</span></h4>'
+					+'<p class="notInTower">'
+					+'未打料数量：<span id="notInTower">'+inbill[i].notInTower+'</span>吨'
+					+'</p>'
+					+'<p class="readyToTower">'
+					+'准备打料数量：<span id="readyToTower">'+inbill[i].notInTower+'</span>吨'
+					+'</p>'
+					+'</div> </a>'
+					+'</li>'
+					+'<ul class="um-list allTower">'
+					+'<li class="towers">'
+					+'<div class="tower">'
+					+'<select class ="siloSelect"></select>'
+					+'</div>'
+					+'<div class="num">'
+					+'数量 <input type="number"/> 吨'
+					+'</div>'
+					+'</li>'
+					+'<li>'
+		           	+'<div>'
+		           	+'<a href="#" class="ti-plus"></a>'
+		            +'</div>'
+					+'<div>'
+					+'<a href="#" class="ti-minus"></a>'
+					+'</div>'
+					+'</li>'
+					+'</ul>'
+					+'<div>';
+			}
+		}else{//无列表信息弹出提示并绘制空列表
+			html+='<a href="#"  class="um-list-item list_item" >'
+					+'		<div class="um-list-item-media">'
+					+'		</div>'
+					+'		<div class="um-list-item-inner">'
+					+'			<div class="um-list-item-body">'
+					+'				<h4 class="um-media-heading f18" style="color:red">当前场内所有饲料已经打入料塔</h4>'
+					+'			</div>'
+					+'		</div> </a>'
+		}
+		$("#inbillListul").html(html);
+		feedAddToTower.initsiloSelect();
+		feedAddToTower.bindEvent();
+	}
+}
 /**
  * 接口回调模块 
  */
-function callBack(arg){
+function callBack(args){
 	summer.hideProgress();
-	if(arg.status == "0"){
-		feedDetails.loadPage(arg.data);
-		feedDetails.data = arg.data.billinfo.inbill;
-	}else{
-		alert("初始化失败"+args.message);
-	}	
+	if(args.status == "0"){
+		feedAddToTower.loadPage(args.data);
+	}else if(args.status == "1"){
+		UM.alert("初始化失败:"+args.message);
+		summer.closeWin();
+	} else {
+		UM.alert(args.message);	
+		summer.closeWin();
+	}		
 }
-function confirmcallBack(arg){
+function confirmcallBack(args){
 	summer.hideProgress();
-	//alert(JSON.stringify(arg));
-	if(arg.status == "0"){
-		feedDetails.init();
+	if(args.status == "0"){
+		feedAddToTower.init();
+		UM.alert("确认成功")
 	}else{
-		alert("确认失败"+arg.message);
-	}
-}
-function updatecallBack(arg){
-	summer.hideProgress();
-	if(arg.status == "0"){
-		alert("修改成功");
-	}else{
-		alert("修改失败"+arg.message);
+		UM.alert("确认失败："+args.message);
 	}
 }
 function erresg(arg){
 	summer.hideProgress();
-	alert("系统运行异常"+JSON.stringify(args));
+	UM.alert("网络异常！");
 }
-
-
-summerready = function () {    
-    
-    feedDetails.init();
-	feedDetails.bindEvent();
+summerready = function(){
+	feedAddToTower.init();
+	feedAddToTower.bindEvent();
 };
