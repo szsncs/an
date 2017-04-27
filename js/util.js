d72edbaaf3e1ebfb6523fd6d4a74afc9a229e424
szsncs <787239289@qq.com>
2017-04-27 21:59:19 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -29,8 +29,8 @@ function isOs(){
 	}
 }
 
-//var https=isOs()=="android"?"file:///android_asset/www/js/":isOs()=="ios"?"file://"+window.location.pathname.split("UMP_Project.app/www/")[0]+"UMP_Project.app/www/js/":"E:/FJSN_ZSYZWorkspace/ZSYZ/js/";
-var https="https://szsncs.github.io/an/js/";
+var https=isOs()=="android"?"file:///android_asset/www/js/":isOs()=="ios"?"file://"+window.location.pathname.split("UMP_Project.app/www/")[0]+"UMP_Project.app/www/js/":"E:/FJSN_ZSYZWorkspace/ZSYZ/js/";
+//var https="https://szsncs.github.io/an/js/";
 var js=".js";
 var jquery_2_1_4="jquery-2.1.4.min";
 var jquery="jquery.min";
