78179e853e53e9b97782a6589f95521eb25c6339
szsncs <787239289@qq.com>
2017-06-27 13:43:18 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -30,9 +30,9 @@ function isOs(){
 }
 
 var andriodUrl="http://222.78.251.20:8412/umserver/ZSYZ.apk";
-var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=53714&fileName=ZSYZ.plist";
-var andriodVersion="1007";
-var iosVersion="1.0.7"
+var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=54030&fileName=ZSYZ.plist";
+var andriodVersion="1008";
+var iosVersion="1.0.8"
 
 var https=isOs()=="android"?"file:///android_asset/www/js/":isOs()=="ios"?"file://"+window.location.pathname.split("www/")[0]+"www/js/":"E:/FJSN_ZSYZWorkspace/ZSYZ/js/";
 //var https="https://szsncs.github.io/an/js/";
